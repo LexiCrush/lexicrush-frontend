aa4be56cfe95d3db61de8ce74b39bd0ced1c31f0
ak2k2 <armaan.kapoor@cooper.edu>
2023-05-01 05:00:21 -0400

    connect login form to server with axios

--- src/Auth.js
+++ src/Auth.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import './Auth.css';
 import { Navigate } from "react-router-dom";
@@ -7,7 +8,61 @@ function Auth() {
 
   const [goToGame, setGoToGame] = React.useState(false);
   if (goToGame) {
-      return <Navigate to="/gamepage" />;
+    return <Navigate to="/gamepage" />;
+  }
+
+  const handleRegister = (evt) => {
+    evt.preventDefault();
+    const username = evt.target.username.value;
+    const password = evt.target.password.value;
+
+    axios.post('http://localhost:8080/auth/register', {
+      username: username,
+      password: password,
+    }, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+      .then((response) => {
+        const username = response.data;
+        if(username){
+            alert(`Success! ${username} has been registered with Lexicrush`);
+        } else {
+            alert(`An account with that username already exists`);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const handleLogin = (evt) => {
+    evt.preventDefault();
+    const username = evt.target.username.value;
+    const password = evt.target.password.value;
+
+    axios.post('http://localhost:8080/auth/login', {
+      username: username,
+      password: password,
+    }, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+      .then((response) => {
+        const token = response.data;
+        if(token.includes("|")){
+            localStorage.setItem('token', token);
+            alert(`Login successful!`);
+            setGoToGame(true);
+        } else {
+            alert(`Invalid username or password`);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
@@ -15,28 +70,28 @@ function Auth() {
       <input type="checkbox" id="chk" aria-hidden="true" />
 
       <div className="signup">
-        <form>
+        <form onSubmit={handleRegister}>
           <label htmlFor="chk" aria-hidden="true">
             Register
           </label>
-          <input type="text" name="txt" placeholder="Username" required="" class="my-input"/>
-          <input type="password" name="pswd" placeholder="Password" required="" class="my-input"/>
+          <input type="text" name="username" placeholder="Username" required="" class="my-input" />
+          <input type="password" name="password" placeholder="Password" required="" class="my-input" />
           <button className='sign-button'>Sign up</button>
         </form>
       </div>
 
       <div className="log">
-        <form>
+        <form onSubmit={handleLogin}>
           <label htmlFor="chk" aria-hidden="true">
             Login
           </label>
-          <input type="text" name="txt" placeholder="Username" required="" class="my-input"/>
-          <input type="password" name="pswd" placeholder="Password" required="" class="my-input"/>
-          <button className='sign-button' onClick={() => {setGoToGame(true)}}>Login</button>
+          <input type="text" name="username" placeholder="Username" required="" class="my-input" />
+          <input type="password" name="password" placeholder="Password" required="" class="my-input" />
+          <button className='sign-button'>Login</button>
         </form>
       </div>
     </div>
-    
+
   );
 }
 
--- src/Longest.js
+++ src/Longest.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import "./Longest.css";
 import { Navigate } from "react-router-dom";
 import axios from 'axios';
