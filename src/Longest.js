db29ec524e7f1938a50a9ee6276c74f70af9cdc8
ak2k2 <armaan.kapoor@cooper.edu>
2023-05-05 23:41:43 -0400

    refactor (-100 lines of redundant components)

--- src/Longest.js
+++ src/Longest.js
@@ -1,216 +1,135 @@
 import React, { useState, useEffect } from 'react';
 import "./Longest.css";
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-import { set } from 'animejs';
 
 function Longest() {
+  const navigate = useNavigate();
   const accessToken = localStorage.getItem('token');
-  const [goBack, setGoBack] = React.useState(false);
+  const username = accessToken.split('|')[0];
+
+  // Game status
+  const [round, setRound] = useState(1);
+  const [roundResult, setRoundResult] = useState('');
   const [currentQuestion, setCurrentQuestion] = useState(() => {
     const storedQuestion = localStorage.getItem('currentQuestion');
     return storedQuestion !== null ? storedQuestion : '';
   });
-  const [points, setPoints] = useState(0);
-  const [roundResult, setRoundResult] = useState('');
-  const [botAnswer, setBotAnswer] = useState('');
-  const [hint, setHint] = useState('');
-  const [currentAnswer, setAnswer] = useState('');
-  const [helloVisible, setHelloVisible] = useState(false);
-  const [userInput, setUserInput] = useState('');
-  const [hintVisible, setHintVisible] = useState(false);
-  const [remainingTime, setRemainingTime] = useState(10);
-  const [restart, setRestart] = useState(false);
-  const [goExit, setExit] = React.useState(false);
   const [currentScore, setCurrentScore] = React.useState(0);
-  const [endGame, setEndGame] = React.useState(false);
-  const [text, setText] = useState("");
+  const [remainingTime, setRemainingTime] = useState(10);
 
-  const token = localStorage.getItem('token');
-  const username = accessToken.split('|')[0];
-  const userhandle = "@" + username;
-  // const URL = 'http://lexicrush.zyns.com';
+  // Answers
+  const [currentAnswer, setCurrentAnswer] = useState('');
+  const [botAnswer, setBotAnswer] = useState('');
+  const [helloVisible, setHelloVisible] = useState(false);
 
-  const [round, setRound] = useState(() => {
-    const storedRound = localStorage.getItem('round');
-    return storedRound !== null ? parseInt(storedRound) : 1;
-  });
-
-  useEffect(() => {
-    localStorage.setItem('round', round);
-  }, [round]);
-
-
+  // Pending status
+  const [pending, setPending] = useState(false);
+  const [refreshScore, setRefreshScores] = useState(false);
 
 
   function handleTimeOver() {
     if (round === 5) {
-      setRound(1)
-      setHelloVisible(false); // hide the answer
-      setHintVisible(false); // hide the hint
-      setCurrentQuestion(''); // clear the current question
-      setPoints(0); // reset the points
-      setRemainingTime(10); // reset the timer
-      setBotAnswer(''); // clear the bot answer
-      setRoundResult(''); // clear the round result
-      setGoBack(true);
+      handleEndGame();
+      navigate('/test');
     } else {
+      setCurrentAnswer("");
       setRound(round + 1); // increase the round
       setRemainingTime(10); // reset the timer
-      setCurrentQuestion(''); // clear the current question
       setBotAnswer(''); // clear the bot answer
       setRoundResult(''); // clear the round result
-      setHelloVisible('')
+      setHelloVisible(false);
     }
   }
 
   useEffect(() => { // get a new question
-    if (currentQuestion === '') {
-      axios.get('http://localhost:8080/api/getq')
-        .then(response => {
-          setCurrentQuestion(response.data);
-          localStorage.setItem('currentQuestion', response.data);
-        })
-        .catch(error => console.error(error));
-    }
+    console.log('New Round: ' + round);
+    axios.get('http://localhost:8080/api/getq')
+      .then(response => {
+        setCurrentQuestion(response.data);
+        localStorage.setItem('currentQuestion', response.data);
+      })
+      .catch(error => console.error(error));
   }, [round]);
 
-  useEffect(() => { // retrieve a hint
-    axios.get('http://localhost:8080/api/hint', {
-      params: {
-        question: currentQuestion
-      }
-    }, {
-    })
-      .then(response => {
-        setHint(response.data);
-        console.log('Hint:', hint);
+  // Get bot answer when enter is pressed
+  useEffect(() => {
+    if (pending && botAnswer.length === 0) {
+      console.log('Enter pressed');
+      axios.get('http://localhost:8080/api/bot', {
+        params: {
+          question: currentQuestion
+        }
+      }, {
       })
-      .catch(error => console.error(error));
-  }, [currentQuestion]);
+        .then(response => {
+          setBotAnswer(response.data);
+          console.log('Bot Answer:', botAnswer);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, [pending])
 
-  const handleChange = (e) => {
-    setAnswer(e.target.value); // this is the text in the input field
-    setText(e.target.value);
+  // Handle scoring when bot answer is retreived
+  useEffect(() => {
+    if (pending && currentAnswer.length > 0 && botAnswer.length > 0 ) {
+      console.log('Handle score');
+      // console.log('Player Answer:', currentAnswer);
+      // console.log('Bots Answer:', botAnswer);
 
-  }
-
-  // useEffect(() => { // if the user gets the answer correct, the bot will respond
-  //   if (points > 0) {
-  //     axios.get('http://localhost:8080/api/bot', {
-  //       params: {
-  //         question: currentQuestion
-  //       }
-  //     }, {
-  //     })
-  //       .then(response => {
-  //         setBotAnswer(response.data);
-  //         console.log('Bot Answer:', botAnswer);
-  //         // setTimeout(() => {
-  //         //   window.location.reload();
-  //         // }, 2000);
-  //       })
-  //       .catch(error => console.error(error));
-  //   }
-  // }, [points]);
-
-  const getBotAns = () => { // get the bot answer
-    axios.get('http://localhost:8080/api/bot', {
-      params: {
+      axios.post('http://localhost:8080/api/updateCurrentScore', {
+        playerAnswer: currentAnswer,
+        botAnswer: botAnswer,
         question: currentQuestion
-      }
-    }, {
-    })
-      .then(response => {
-        setBotAnswer(response.data);
-        console.log('Bot Answer:', botAnswer);
-        // setTimeout(() => {
-        //   window.location.reload();
-        // }, 2000);
+      }, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Access-Token': accessToken,
+        }
       })
-      .catch(error => console.error(error));
+        .then(response => {
+          console.log(response);
+          setRoundResult(response.data);
+          setRefreshScores(true);
+        })
+        .catch(error => {
+          setRefreshScores(false);
+          console.error(error)
+        });
+    }
+  }, [botAnswer])
+
+  // Fetch the updated current score from backend
+  useEffect(() => {
+    if (refreshScore) {
+      console.log('Get updated score' + accessToken);
+      axios.get('http://localhost:8080/api/getCurrentScore',{
+        headers: {
+          'Access-Token': accessToken,
+        }
+      })
+        .then(response => {
+          setCurrentScore(response.data);
+          setRefreshScores(false);
+          setHelloVisible(true);
+          setPending(false);
+        })
+        .catch(error => console.error(error));
+ 
+      // console.log('Current Score:', currentScore);
+    }
+  }, [refreshScore])
+
+  const handleAnswerChange = (e) => {
+    setCurrentAnswer(e.target.value); // this is the text in the input field
   }
-
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log('Question:', currentQuestion);
-  //   console.log('User Submitted:', currentAnswer);
-
-  //   axios.post('http://localhost:8080/api/checkans', {
-  //     question: currentQuestion,
-  //     answer: currentAnswer
-  //   }, {
-  //     headers: {
-  //       'Content-Type': 'application/x-www-form-urlencoded'
-  //     }
-  //   })
-  //     .then(response => { console.log(response); setPoints(response.data); })
-  //     .catch(error => console.error(error));
-  // }
-
-  const handleScoring = (e) => {
-    e.preventDefault();
-    console.log('Player Answer:', currentAnswer);
-    console.log('Bots Answer:', botAnswer);
-
-    axios.post('http://localhost:8080/api/updateCurrentScore', {
-      playerAnswer: currentAnswer,
-      botAnswer: botAnswer,
-      question: currentQuestion
-    }, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Access-Token': accessToken,
-        // 'Access-Control-Allow-Origin': '*', // allow CORS
-        // 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-        // 'Access-Control-Allow-Origin': 'http://localhost:3000',
-        // 'Access-Control-Allow-Credentials': 'true'
-      }
-    })
-      .then(response => { console.log(response); setRoundResult(response.data); })
-      .catch(error => console.error(error));
-      // if roundResult is not "Invalid access token"
-      // then get the current score
-      // if (roundResult !== "Invalid access token") {
-      //   getBotAns();
-      // }
-
-    // axios.get('http://localhost:8080/api/getCurrentScore', {
-    //   headers: {
-    //     'Access-Token': accessToken
-    //   }
-    // })
-    //   .then(response => { console.log(response); setCurrentScore(response.data); })
-    //   .catch(error => console.error(error));
-
-    // console.log('Current Score:', currentScore);
-  }
-
-
-  const getCurrentScore = () => {
-    axios.get('http://localhost:8080/api/getCurrentScore',{
-      headers: {
-        'Access-Token': accessToken,
-      }
-    })
-      .then(response => { console.log(response); setCurrentScore(response.data); })
-      .catch(error => console.error(error));
-
-    console.log('Current Score:', currentScore);
-  }
-
 
   const handleKeyDown = (e) => {
     if (e.keyCode === 13) { // Enter key
       e.preventDefault(); // prevent the default action (submitting the form) when pressing enter key in input field
-      setUserInput(currentAnswer);
-      getBotAns();
-      handleScoring(e);
-      getCurrentScore();
-      setHelloVisible(true);
-      console.log('Submitted:', currentAnswer);
-      setText("")
+      setPending(true);
     }
   }
 
@@ -224,41 +143,12 @@ function Longest() {
         'Access-Token': accessToken
       }
     })
-      .then(response => { console.log(response); setEndGame(response.data); })
+      .then(response => {
+        console.log(response);
+        navigate('/test')
+      })
       .catch(error => console.error(error));
   }
-
-  if (goBack) {
-    handleEndGame();
-    return <Navigate to="/test" />;
-  }
-
-  // if (points > 0) {
-  //   handleScoring();
-  //   getCurrentScore();
-  // }
-
-  function handlegoExit() {
-    setExit(true);
-  }
-
-  if (goExit) {
-    handleEndGame();
-    return <Navigate to="/profile" />;
-  }
-
-  // if (accessToken) {
-
-  //   const tokenTime = accessToken.split('|')[1];
-  //   const currentTime = new Date().getTime();
-
-  //   if (currentTime - tokenTime > 1000 * 60 * 60 * 2) { // if token is older than 2 hours
-  //     localStorage.removeItem('token'); // remove token from local storage
-  //     alert('Your session has expired. Please log in again.'); // alert user
-  //     return <Navigate to="/auth" />; // redirect to login page
-
-  //   } 
-  // }
 
 
   return (
@@ -278,10 +168,10 @@ function Longest() {
       </div>
       <div className="rectangle-left">
         <h1 class="player-name">{username}</h1>
-        <button onClick={() => { setHintVisible(true) }} className="hint-button" style={{ display: 'block', marginBottom: '10px', fontFamily: 'ButtonFont' }}>
+        {/* <button onClick={() => { setHintVisible(true) }} className="hint-button" style={{ display: 'block', marginBottom: '10px', fontFamily: 'ButtonFont' }}>
           {hint}
-        </button>
-        <button onClick={() => { handlegoExit(true) }} className="exit-button" style={{ display: 'block', marginBottom: '10px', fontFamily: 'ButtonFont' }}>
+        </button> */}
+        <button onClick={() => { handleEndGame() }} className="exit-button" style={{ display: 'block', marginBottom: '10px', fontFamily: 'ButtonFont' }}>
           Exit
         </button>
       </div>
@@ -312,7 +202,7 @@ function Longest() {
       </div>
       <p>
         <span className="xx"> {/* user text input box */}
-          <input type="text" value={text} placeholder="Enter" onKeyDown={handleKeyDown} onChange={handleChange} />
+          <input type="text" value={currentAnswer} placeholder="Enter" onKeyDown={handleKeyDown} onChange={handleAnswerChange} />
           <span class="my-span"></span>
         </span>
       </p>
@@ -374,4 +264,3 @@ function Time(props) {
 }
 
 export default Longest;
-
